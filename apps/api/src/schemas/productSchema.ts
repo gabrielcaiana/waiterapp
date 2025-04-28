265073d879f34bcd0aa6c2ec3072967a293cfcf8
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-04-28 19:43:36 -0300

    feat(schemas): add zod schemas for category and product
    Introduce zod validation schemas for category and product entities to enforce data integrity and validation rules. This ensures consistent and valid data handling across the application.

---
+++ apps/api/src/schemas/productSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const productSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  description: z.string().min(1, 'Description is required'),
+  imagePath: z.string().min(1, 'ImagePath is required'),
+  price: z.number().min(1, 'Price is required'),
+  ingredients: z
+    .array(
+      z.object({
+        name: z.string().min(1, 'Ingredient name is required'),
+        icon: z.string().min(1, 'Ingredient icon is required'),
+      }),
+    )
+    .min(1, 'At least one ingredient is required'),
+  category: z.string().min(1, 'Category is required'),
+});
+
+export type Product = z.infer<typeof productSchema>;

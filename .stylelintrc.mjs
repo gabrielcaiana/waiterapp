9d7acfcef333569d0386a5f451bd001d4f712e7b
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-05-02 07:41:09 -0300

    chore(web): config tailwind and monorepo configs

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -3,9 +3,6 @@ export default {
   plugins: ['stylelint-order'],
   rules: {
     'order/properties-alphabetical-order': true,
-    'declaration-colon-space-after': null,
-    'max-empty-lines': 2,
-    indentation: 2,
     'at-rule-no-unknown': null,
     'no-empty-source': null,
   },
